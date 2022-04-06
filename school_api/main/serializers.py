b337378ecae443c76b54f9ab0e3cbbbf9d8da860
Mathieu Piton <mathieu.piton@credit-municipal-services.fr>
2022-04-06 20:40:10 +0200

    Model serializer

---
+++ school_api/main/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Teacher, CourseCategory, Course, Student
+
+
+# This class is a serializer for the Teacher model
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
--- school_api/main/views.py
+++ school_api/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Teacher
+from .serializers import TeacherSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class TeacherList(APIView):
+    def get(self, request):
+        """
+        Get all teachers and return them in JSON format
+
+        :param request: The HTTP request that triggered this view
+        :return: A JSON object containing a list of all the teachers.
+        """
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data)
